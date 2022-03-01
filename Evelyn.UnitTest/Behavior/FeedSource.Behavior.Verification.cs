59185ff128e0d2f410fd8270fecd0bbf7d876062
陈宏葆 <chenhongbao@outlook.com>
2022-03-01 17:01:32 +0800

    Add unit tests methods for feed source.

---
+++ Evelyn.UnitTest/Behavior/FeedSource.Behavior.Verification.cs
@@ -0,0 +1,42 @@
+﻿/*
+Copyright (C) 2022  Chen Hongbao<chenhongbao@outlook.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Evelyn.UnitTest.Behavior
+{
+    [TestClass]
+    public class FeedSourceBehaviorVerification
+    {
+        [TestMethod("Subscribe for an instrument many times.")]
+        public void SubscribeManyTimes()
+        {
+
+        }
+
+        [TestMethod("Unsubscribe for an instrument many times.")]
+        public void UnsubscribeManyTimes()
+        {
+
+        }
+
+        [TestMethod("Unsubscribe an unknown instrument.")]
+        public void UnsubscribeUnknownInstrument()
+        {
+
+        }
+    }
+}
