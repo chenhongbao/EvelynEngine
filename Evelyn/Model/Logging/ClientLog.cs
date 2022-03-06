3c62b5eefcf89da8a2045e22ca7c9332e10de703
陈宏葆 <chenhongbao@outlook.com>
2022-03-06 08:12:28 +0800

    Rearrage namespaces.

--- Evelyn/Model/Logging/ClientLog.cs
+++ Evelyn/Model/Logging/ClientLog.cs
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 using Microsoft.Extensions.Logging;
 
-namespace Evelyn.Model.CLI
+namespace Evelyn.Model.Logging
 {
     public struct ClientLog
     {
